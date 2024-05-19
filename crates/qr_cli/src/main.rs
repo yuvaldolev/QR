3bb05cdc6b9ee2dd61ba6df4952f1d724a6b4821
Yuval Dolev <mail@ydolev.com>
2024-05-20 01:51:25 +0300

    feat(cli): retrieve encode API URL using compile-time environment variable

--- crates/qr_cli/src/main.rs
+++ crates/qr_cli/src/main.rs
@@ -1,14 +1,20 @@
-use clap::Parser;
-use cli::Cli;
-// use qr_encoder::Encoder;
+use reqwest::blocking::Client;
 
 mod cli;
 
+const QR_ENCODE_API_URL: &str = env!("QR_ENCODE_API_URL");
+
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+    // let cli = Cli::parse();
 
     // let encoder = Encoder::new(cli.error_correction_level.into_error_correction_level());
     // encoder.encode(&cli.data)?;
+
+    let client = Client::new();
+
+    println!("Sending request to {}", QR_ENCODE_API_URL);
+    let response = client.post(QR_ENCODE_API_URL).send()?;
+    println!("Response: {:?}", response);
 
     Ok(())
 }
